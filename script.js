4ea48782eeeee69da68cd281750c81248bbe33b0
Niama7 <167180843+Niama7@users.noreply.github.com>
2025-01-27 19:16:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Toggle menu visibility
-    menuToggle.classList.toggle('open'); // Toggle the "open" class to switch icon
-});
